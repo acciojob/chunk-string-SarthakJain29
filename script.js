646edf9767acaea05610c8aa077764d560d5e58b
Sarthak Jain <93116216+SarthakJain29@users.noreply.github.com>
2025-07-17 12:21:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function stringChop(str, size) {
 	let res = [];
 	let curr = 0;
 	let temp = "";
-	if(str.length === null) return [];
+	if (!str || typeof len !== 'number' || len <= 0) return [];
 	for(let i=0;i<str.length;i++){
 		if(curr === size){
 			res.push(temp);
